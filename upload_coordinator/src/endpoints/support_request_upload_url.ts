ac713d097f078b5ec5181be39568936e02924c04
agsheeran <asheeran@aiscorp.com>
2024-06-27 11:47:04 -0400

    Jared do this

--- upload_coordinator/src/endpoints/support_request_upload_url.ts
+++ upload_coordinator/src/endpoints/support_request_upload_url.ts
@@ -1,6 +1,13 @@
 import exp from "constants";
 import { Request, Response } from "express";
+import * as crypto from "crypto";
+
+
 
 export async function post_support_request_upload_url(req: Request, res: Response ) {
-    
+    // Get CaseID from request body
+    // Generate New GUID use 
+    const uuid: string = crypto.randomUUID();
+    // Create entry in cases table that contains the caseID and guid... the timestamp gets assigned by MariaDB
+    // Return the GUID in the response
 }
