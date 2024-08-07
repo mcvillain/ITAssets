fd104f3e27c240215d1b0552837cc03370abf0c6
Stebaniel <113371484+Stebaniel@users.noreply.github.com>
2024-08-07 10:19:05 -0400

    descriptions added to the about page

--- frontend/webapp/src/components/aboutPage.vue
+++ frontend/webapp/src/components/aboutPage.vue
@@ -1,12 +1,15 @@
 <template>
     <notification />
+
+    <!-- 2023 About Page -->
+
     <div style="background: #f1f1f1; color: white;">
         <div class="frame">
             <div class="innerFrame">
                 <img id="groupPhoto" class="picture2023" src="/src/assets/Intern-Group-Photo-Good.png"
                     @click="resizePhoto()" />
                 <div class="textA2023">
-                    <p id="heading" style="color: white;">2023 IT Asset Project</p>
+                    <p class="heading" style="color: white;">2023 IT Asset Project</p>
                     <p style="color: white; font-size: 2rem;">{{ version }}</p>
                     Designed by La Salle College High School Class of 2024 Summer Interns:
                     <div class="textB2023">
@@ -19,6 +22,9 @@
             </div>
         </div>
         <div style="margin: 6rem;"></div>
+
+        <!-- Descriptions for the 2023 It Assets Page: -->
+
         <div class="p">
             <div class="innerP">Backend</div>
         </div>
@@ -75,18 +81,21 @@
         </div>
 
         <div style="margin: 3rem;">
-            <img class="flowchart2023" src="/src/assets/IT-Asset-Flowchart.png"  />
-            <img class="flowchart2023" src="/src/assets/2023ITAssetsPage.png" />
+            <img class="flowchart2023" src="/src/assets/IT-Asset-Flowchart.png" />
+            <img class="page2023" src="/src/assets/2023ITAssetsPage.png" />
         </div>
+
+        <!-- 2024 About Page: -->
 
         <div class="frame">
             <div class="innerFrame">
-                <img id="groupPhoto" class="picture2024" src="/src/components/router/cheese.gif" />
+                <img id="groupPhoto" class="picture2024" src="/src/assets/groupPhoto2024.jpg" />
                 <div class="textA2024">
-                    <p id="heading" style="color: white;">2024 IT Asset Project (Coming Soon)</p>
-                    Designed by La Salle College High School Class of 2024 Summer Interns:
+                    <p class="heading" style="color: white;">2024 IT Asset Project</p>
+                    <p style="color: white; font-size: 2rem;">{{ version }}</p>
+                    Designed by the Aegis Software 2024 Summer Interns:
                     <div class="textB2024">
-                        Ethan Pakuris, Jared Werts, Ian Whitaker, Evan
+                        Aiden Sheeran, Brey Rivera, Ethan Pakuris, Jared Werts, Ian Whitaker, Evan
                         Comiciotto, Fahd Khan, and
                         <a style="color: white;" id="stName" href="partymode=true">Stephen Trezza</a> <br>
 
@@ -95,10 +104,88 @@
             </div>
         </div>
 
+        <!-- Description for what the two teams did during their internship  -->
+        <!-- Pay No attention to the class names. When I originally made it the names made sense -->
 
+        <div class="teams">
+            <div class="aiden">
+                <div class="teamFrameOrange"> Team Aiden
+                    <div class="textB2024" style="line-height: 2;">
+
+                        <!-- Team Description Goes Here: -->
+
+                        
+
+                        We have two types of customers:
+
+                        AIS/AZ customers who interact with our system via the AIS/AZ Customer_Facing Services to
+                        upload their files.
+                        ITAR Customers who deal with sensitive data. They use the
+                        AIS/AZ Traffic_Facing Services which ensures compliance with ITAR regulations. <br> <br>
+
+                        Our system utilizes two distinct cloud storage solutions: <br>
+
+                        Azure: For our regular AIS/AZ customers, providing reliable and scalable storage. <br>
+                        Azure Government SMB: A specialized solution for our ITAR customers, offering enhanced security
+                        features to meet strict regulatory requirements. <br> <br>
+
+                        Once the files are uploaded, they are handled by our Uploader
+                        Service. This service is the backbone of our system, ensuring that files are processed and
+                        stored correctly in our Checkout Data database.
+
+                    </div>
+                </div>
+            </div>
+            <div class="brey">
+                <div class="teamFrameWhite">
+                    <div class="textB2024">
+                        <img class="aidenflowchart2024" src="/src/assets/uploaderFlowChart.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="teams" style="padding-top: 1rem;">
+            <div class="brey">
+                <div class="teamFrameOrange" style="background: white;">
+                    <div class="textB2024">
+                        <img class="breyflowchart2024" src="/src/assets/teamBreyFlowChart.png">
+                    </div>
+                </div>
+            </div>
+            <div class="aiden">
+                <div class="teamFrameWhite" style="background: linear-gradient(to right, #f26522, #ef3b32);"> Team Brey
+                    <div class="textB2024">
+
+                        <!-- Team Description Goes Here: -->
+
+                        Saas Page
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Where the Interns are going to College -->
+
+        <div class="college" style="background: linear-gradient(to right, #27272d, #34343b, #5d5d6d);">
+            <div class="collegeFrame"> Where Our Interns Went to College:
+                <div>
+                    <img class="collegePic1" src="/src/assets/NortheasternLogo.png">
+                    <img class="collegePic1" src="/src/assets/TempleLogo.png">
+                    <img class="collegePic1" src="/src/assets/VillanovaLogo.png">
+                    <img class="collegePic1" src="/src/assets/PittLogo.png">
+                </div>
+                <div>
+                    <img class="collegePic1" src="/src/assets/BC-Logo.png">
+                    <img src="/src/assets/arcadia-university-logo.png" style="margin-bottom: 2rem;">
+                    <img class="collegePic1" src="/src/assets/gwynedd.png" style="margin-bottom: 0rem;">
+                </div>
+            </div>
+        </div>
 
         <div class="collegeGrid" style="background: linear-gradient(to right, #f26522, #ef3b32);">
-            <div class="aidenTeam" style="font-size: 50px;"> Team Aiden
+            <div class="aidenTeam" style="font-size: 50px;">
 
                 <div>
                     <div class="slider">
@@ -106,122 +193,15 @@
                         <img id="groupPhoto" class="collegePic2" src="/src/assets/MoreFahd.png" />
                         <img id="groupPhoto" class="collegePic2" src="/src/assets/EvenMoreFahd.png" />
                     </div>
-                    <img class="collegePic1" src="/src/assets/TempleLogo.png">
+                    
                     <div class="picFrame"></div>
                     <div class="picFrame2"></div>
-                </div>
-                <div>
-                    <div class="slider">
-                        <img id="groupPhoto" class="collegePic2" src="/src/assets/StephenAboutPagePic.png" />
-                        <img id="groupPhoto" class="collegePic2" src="/src/assets/MoreFahd.png" />
-                        <img id="groupPhoto" class="collegePic2" src="/src/assets/EvenMoreFahd.png" />
-                    </div>
-                    <img class="collegePic1" src="/src/assets/VillanovaLogo.png">
-                    <div class="picFrame"></div>
-                    <div class="picFrame2"></div>
-                </div>
-                <div>
-                    <div class="slider">
-                        <img id="groupPhoto" class="collegePic2" src="/src/assets/FahdAboutPagePic.png" />
-                        <img id="groupPhoto" class="collegePic2" src="/src/assets/MoreFahd.png" />
-                        <img id="groupPhoto" class="collegePic2" src="/src/assets/EvenMoreFahd.png" />
-                    </div>
-                    <img class="collegePic1" src="/src/assets/NortheasternLogo.png">
-                    <div class="picFrame"></div>
-                    <div class="picFrame2"></div>
-
-                </div>
-            </div>
-            <div class="breyTeam" style="font-size: 50px;"> Team Brey
-                <div>
-                    <img class="collegePic1" src="/src/assets/PittLogo.png">
-                    <img id="groupPhoto" class="collegePic2" src="/src/assets/EvanAboutPagePic.png" />
-                </div>
-                <div>
-                    <img class="collegePic1" src="/src/assets/PittLogo.png">
-                    <img id="groupPhoto" class="collegePic2" src="/src/assets/IanAboutPagePic.png" />
-                </div>
-                <div>
-                    <img class="collegePic1" src="/src/assets/BC-Logo.png">
-                    <img id="groupPhoto" class="collegePic2" src="/src/assets/EthanAboutPagePic.png" />
                 </div>
             </div>
         </div>
 
         <div style="margin: 3rem;"></div>
 
-        <!-- <p id="heading">Old Page (with some tweaks):</p>
-
-        <table id="descriptions" style="color: black;">
-            <tr>
-                <th id="tableHeading" colspan="4">
-                    Core functionality: Gathers server and database information from
-                    various hosts, compiles them, and displays them on a webpage.
-                </th>
-            </tr>
-            <tr id="headerLabels">
-                <th class="tableLabel">Backend</th>
-                <th class="tableLabel">Authentication</th>
-                <th class="tableLabel">Frontend</th>
-                <th class="talbeLabel">Distribution</th>
-            </tr>
-            <tr>
-                <td class="description">
-                    Gathers data from a series of automated Powershell scripts that
-                    collect the data and send it to a set host in json format.
-                    The host runs a series of programs that compile each set of data
-                    into separate lists, checking for outdated information and
-                    deleting it. The backend then utilizes the get and post APIs in
-                    order to store and transmit the data to the frontend.
-                </td>
-                <td class="description">
-                    Upon frontend fetch request, the login API checks the provided
-                    username and password. If they are valid, the API will return a
-                    token that is stored in a cookie and passed to authentication
-                    API. If the token is valid, the auth API allows the page to
-                    load.
-                </td>
-                <td class="description">
-                    Utilizes Vue3 and fetch APIs to retrieve the data from the set
-                    host. The data is then displayed in a series of tables which can
-                    be filtered through the collapsible sidebar and searched as
-                    desired.
-                </td>
-                <td class="description">
-                    The code is all compiled in Docker containers that are hosted
-                    and stored on a virtual server in Azure to ensure the programs
-                    can run constantly and be accessed as needed.
-                </td>
-            </tr>
-            <tr>
-                <td class="people">Jared, Fahd, Ethan</td>
-                <td class="people">Stephen</td>
-                <td class="people">Evan, Ian, Stephen, Ethan</td>
-                <td class="people">Jared</td>
-            </tr>
-        </table>
-        <div style="margin: 3rem;"></div>
-        <div class="row">
-            <div class="collumn">
-                <img id="flowchart" src="/src/assets/IT-Asset-Flowchart.png" @click="resizeFlowChart()" />
-                <figcaption>
-                    Flowchart showing capabilities and path of data
-                </figcaption>
-            </div>
-            <div class="collumn">
-                <img id="groupPhoto" src="/src/assets/Intern-Group-Photo-Good.png" @click="resizePhoto()" />
-                <p>
-                    Designed by La Salle College High School Class of 2024 Summer
-                    Interns
-                </p>
-                <p class="internList">
-                    Members: Ethan Pakuris, Jared Werts, Ian Whitaker, Evan
-                    Comiciotto, Fahd Khan, and
-                    <a id="stName" href="partymode=true">Stephen Trezza</a>
-                </p>
-                <p class="finePrint">(From left to right as pictured)</p>
-            </div>
-        </div> -->
     </div>
 </template>
 
@@ -255,5 +235,3 @@ export default {
     },
 };
 </script>
-
-
