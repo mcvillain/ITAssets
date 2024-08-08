b50907d2d42433a550310fc4e019843cde814f63
McVillain <coreymcvillain@gmail.com>
2024-08-08 14:46:31 -0400

    Update index.ts
    changed the payload limit on the app.use(express.json()) call.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -50,7 +50,7 @@ const session_config: session.SessionOptions = {
 };
 
 // Middleware
-app.use(express.json());
+app.use(express.json( {limit: "10mb" } ));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session(session_config));
