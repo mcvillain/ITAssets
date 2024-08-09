7d1c8329e17506b923179c7dfbd7d504347013a3
McVillain <coreymcvillain@gmail.com>
2024-08-09 10:40:20 -0400

    Update sql.ts
    IT'S OVER 9000!

--- backend/src/sql.ts
+++ backend/src/sql.ts
@@ -22,7 +22,7 @@ function getPool(): mariadb.Pool {
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASS,
-            connectionLimit: 100,
+            connectionLimit: 10000,
             database: "itassets",
         });
     }
