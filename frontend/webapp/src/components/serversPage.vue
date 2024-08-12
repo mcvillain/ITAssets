3dc82e50aa7e31cfdb8f1bb1639b173731249b75
agsheeran <asheeran@aiscorp.com>
2024-08-12 14:19:43 -0400

    Removed unused comments

--- frontend/webapp/src/components/serversPage.vue
+++ frontend/webapp/src/components/serversPage.vue
@@ -56,22 +56,6 @@ const headers = [
 ];
 const sortBy = ref([{ key: 'size', order: 'desc' }]);
 
-// function formatDate(timestamp) {
-//   // Create a Date object from the timestamp
-//   const date = new Date(timestamp);
-
-//   // Define options for date and time formatting
-//   const options = {
-//     year: '2-digit', month: '2-digit', day: '2-digit',
-//     hour: '2-digit', minute: '2-digit', second: '2-digit'
-//   };
-
-//   // Format the date and time with Intl.DateTimeFormat
-//   return new Intl.DateTimeFormat('en-US', options).format(date);
-// }
-
-
-
 onMounted(() => {
     fetch(import.meta.env.VITE_API_ENDPOINT + "/servers", {
         credentials: "include",
