81e953a13b9a29b12daf6fae84cbbf24b9d9290a
agsheeran <asheeran@aiscorp.com>
2024-08-14 09:32:01 -0400

    Cleared old kubernetes

--- kubernetes/kill-db.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location $PSScriptRoot
-
-kubectl delete -f ./mariadb.yml
-kubectl delete -f ./mariadb-service.yml
-kubectl delete -f ./mariadb-secrets.yml
-kubectl delete -f ./mariadb-data.yml
-kubectl delete -f ./mariadb-data-volume.yml
--- kubernetes/kill.ps1
+++
@@ -1,9 +0,0 @@
-Set-Location $PSScriptRoot
-
-kubectl delete -f ./frontend.yml
-kubectl delete -f ./backend.yml
-kubectl delete -f ./frontend-service.yml
-kubectl delete -f ./backend-service.yml
-kubectl delete -f ./registry.yml
-kubectl delete -f ./frontend-tls-secrets.yml
-kubectl delete -f ./backend-secrets.yml
