f4673d0c0823d4407ba3103a08d011115e130a42
McVillain <coreymcvillain@gmail.com>
2024-09-04 10:31:00 -0400

    Update startup.sh
    more certs

--- aegis_uploader/startup.sh
+++ aegis_uploader/startup.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 mount -t cifs $SMB_PATH /mnt/uploads -o user=$SMB_USER,password=$SMB_PASS,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30
-#NODE_EXTRA_CA_CERTS=node_modules/node_extra_ca_certs_mozilla_bundle/ca_bundle/ca_intermediate_root_bundle.pem
-node index.js
+NODE_EXTRA_CA_CERTS=node_modules/node_extra_ca_certs_mozilla_bundle/ca_bundle/ca_intermediate_root_bundle.pem node index.js
